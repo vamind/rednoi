5968b54bb1f486b48c29e1845ae2d41ecdb443c8
Petr Vacha <pan.vacha@gmail.com>
2017-04-18 23:31:13 +0200

    added retweets

--- src/AppBundle/Service/TwitterAPIService.php
+++ src/AppBundle/Service/TwitterAPIService.php
@@ -5,6 +5,9 @@ namespace AppBundle\Service;
 
 class TwitterAPIService {
 
+    /** @const string */
+    const TWITTERCOM = "https://twitter.com/";
+
     /** @var array $access */
     private $access;
 
@@ -37,9 +40,10 @@ class TwitterAPIService {
                 ->buildOauth($endPoint, $requestMethod)
                 ->performRequest(),$assoc = TRUE);
 
-            foreach ($string['statuses'] as $items) {
+
+            foreach ($string['statuses'] as $i => $items) {
                 $items['query'] = $query;
-                $items['twitterAuthorLink'] = "https://twitter.com/" . $items['user']['screen_name'];
+                $items['twitterAuthorLink'] = self::TWITTERCOM . $items['user']['screen_name'];
                 $items['name'] = $items['user']['name'];
                 $items['twitterName'] = $items['user']['screen_name'];
                 $items['time'] = $this->timeSince($items['created_at']);
@@ -47,20 +51,40 @@ class TwitterAPIService {
 
                 if (isset($items['quoted_status'])) {
                     $items['retweetAuthorName'] = $items['quoted_status']['user']['name'];
-                    $items['retweetAuthorTwitterName'] = $items['quoted_status']['user']['screen_name'];
-                    $items['retweetAuthorLink'] = "https://twitter.com/" . $items['quoted_status']['user']['name'];
-                    $items['retweetLink'] = "https://twitter.com/" . $items['retweetAuthorLink'] . "/status/" . $items['quoted_status']['id_str'];
+                    $items['retweetAuthorTwitterName'] = $items['quoted_status']['user']['name'];
+                    $items['retweetAuthorLink'] = self::TWITTERCOM . $items['quoted_status']['user']['screen_name'];
+                    $items['retweetLink'] = self::TWITTERCOM . $items['retweetAuthorLink'] . "/status/" . $items['quoted_status']['id_str'];
                     if (isset($items['quoted_status']['entities']['media'])) {
                         $items['mediaTw'] = $items['quoted_status']['entities']['media'][0]["media_url_https"];
                     }
                     $items['quoted_status']['full_text'] = $this->processTweet($items['quoted_status']['full_text'], TRUE);
+                    $items['quoted_status']['link'] = $items['retweetAuthorLink'] . "/status/" . $items['quoted_status']['id_str'];
                 }
+
                 if (isset($items['entities']['media'])) {
                     $items['media'] = $items['entities']['media'][0]['media_url'];
                 }
 
                 foreach ($items['entities']['urls'] as $url) {
                     $items['full_text'] = str_replace($url['url'], $url['expanded_url'], $items['full_text']);
+                }
+
+                if (isset($items['retweeted_status']['full_text'])) {
+                    $items['full_text'] = $items['retweeted_status']['full_text'];
+                    $items['RT'] = TRUE;
+                    $items['originRetweetAuthorName'] = $items['retweeted_status']['user']['name'];
+                    $items['originRetweetAuthorTwitterName'] = $items['retweeted_status']['user']['screen_name'];
+                    $items['originRetweetAuthorLink'] = self::TWITTERCOM . $items['retweeted_status']['user']['screen_name'];
+
+                    if (isset($items['retweeted_status']['entities']['media'])) {
+                        $items['media'] = $items['retweeted_status']['entities']['media'][0]['media_url'];
+                    }
+
+                    foreach ($items['retweeted_status']['entities']['urls'] as $url) {
+                        $items['full_text'] = str_replace($url['url'], $url['expanded_url'], $items['full_text']);
+                    }
+                } else {
+                    $items['RT'] = FALSE;
                 }
                 $items['full_text'] = $this->processTweet($items['full_text']);
                 $mergedArray[$items['id']] = $items;
@@ -107,9 +131,9 @@ class TwitterAPIService {
      * @return string
      */
     private function processTweet($text, $retweet = FALSE) : string {
-        $text = preg_replace('/https?:\/\/t\.co\/[^\s]+/', '', $text);
         if (!$retweet) {
-            $text = preg_replace('/[^\/]@([^\s]+)/', '<a href="https://twitter.com/\1" class="at" target="_blank">@</a><a href="https://twitter.com/\1" target="_blank">\1</a>', $text);
+            $text = preg_replace('/https?:\/\/t\.co\/[^\s]+/', '', $text);
+            $text = preg_replace('/([^\/])?@([^\s\,\!\?\)]+[^\s\,\!\?\)\.]+)/', '\1<a href="https://twitter.com/\2" class="at" target="_blank">@</a><a href="https://twitter.com/\2" target="_blank">\2</a>', $text);
         }
         $text = preg_replace('/(\s)+(http:\/\/|https:\/\/)(www\.)?([^\s]+)/', '\1<a href="\2\3\4" target="_blank">\4</a>', $text);
         return $text;
