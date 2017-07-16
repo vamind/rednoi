2b39accc78a83b887b65812e7863b7c1985c7d86
Petr Vacha <pan.vacha@gmail.com>
2017-07-16 18:34:50 +0200

    fixed namespace order + added .coveralls.yml

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use AppBundle\Service\TwitterAPIService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class DefaultController extends Controller
 {
